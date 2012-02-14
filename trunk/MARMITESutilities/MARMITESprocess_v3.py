IMPORTANT FIX!!! Corrected the conversion of EXF from MF into MM units

trunk/MARMITESutilities/MARMITESprocess_v3.py
@@ -199,10 +199,10 @@ class PROCESS:
 
     ######################
 
-    def inputTS(self, NMETEO, NVEG, NSOIL, nstp,
-                inputDate_fn, inputZON_TS_RF_fn,
-                inputZON_TS_PT_fn, inputZON_TS_RFe_fn,
-                inputZON_TS_PE_fn, inputZON_TS_E0_fn
+    def inputSP(self, NMETEO, NVEG, NSOIL, nstp,
+                inputDate_fn, inputZON_SP_RF_fn,
+                inputZON_SP_PT_fn, inputZON_SP_RFe_fn,
+                inputZON_SP_PE_fn, inputZON_SP_E0_fn
                 ):   #IRR_fn
 
         ntotstp = int(sum(nstp))
@@ -246,25 +246,25 @@ class PROCESS:
             raise ValueError, '\nFATAL ERROR!\nThe total area of the vegetation in one cell cannot exceed 100.0%!'
 
         # READ RF for each zone
-        RF_fn=os.path.join(self.MM_ws, inputZON_TS_RF_fn)
+        RF_fn=os.path.join(self.MM_ws, inputZON_SP_RF_fn)
         if os.path.exists(RF_fn):
             RF = np.loadtxt(RF_fn)
         else:
             raise ValueError, "\nFATAL ERROR!\nThe file %s doesn't exist!!!" % RF_fn
-        RFzonesTS=np.zeros([NMETEO,ntotstp], dtype=float)
+        RFzonesSP=np.zeros([NMETEO,ntotstp], dtype=float)
         for n in range(NMETEO):
             for t in range(ntotstp):
-                RFzonesTS[n,t]=RF[n*ntotstp+t]
+                RFzonesSP[n,t]=RF[n*ntotstp+t]
 
-        E0_fn=os.path.join(self.MM_ws, inputZON_TS_E0_fn)
+        E0_fn=os.path.join(self.MM_ws, inputZON_SP_E0_fn)
         if os.path.exists(E0_fn):
             E0 = np.loadtxt(E0_fn)
         else:
             raise ValueError, "\nFATAL ERROR!\nThe file %s doesn't exist!!!" % E0_fn
-        E0zonesTS=np.zeros([NMETEO,ntotstp], dtype=float)
+        E0zonesSP=np.zeros([NMETEO,ntotstp], dtype=float)
         for n in range(NMETEO):
             for t in range(ntotstp):
-                E0zonesTS[n,t]=E0[n*ntotstp+t]
+                E0zonesSP[n,t]=E0[n*ntotstp+t]
 
         ### READ IRR for each zone
         ##IRR_fn=os.path.join(self.MM_ws,IRR_fn)
@@ -280,51 +280,51 @@ class PROCESS:
 
 
         # READ PT for each zone and each vegetation
-        PT_fn = os.path.join(self.MM_ws, inputZON_TS_PT_fn)
+        PT_fn = os.path.join(self.MM_ws, inputZON_SP_PT_fn)
         if os.path.exists(PT_fn):
             PTtmp = np.loadtxt(PT_fn)
         else:
             raise ValueError, "\nFATAL ERROR!\nThe file %s doesn't exist!!!" % PT_fn
-        PTvegzonesTS=np.zeros([NMETEO,NVEG,ntotstp], dtype=float)
+        PTvegzonesSP=np.zeros([NMETEO,NVEG,ntotstp], dtype=float)
         for n in range(NMETEO):
             for v in range(NVEG):
                 for t in range(ntotstp):
-                    PTvegzonesTS[n,v,t]=PTtmp[t+(n*NVEG+v)*ntotstp]
+                    PTvegzonesSP[n,v,t]=PTtmp[t+(n*NVEG+v)*ntotstp]
                     #structure is [number of zones, number of vegetation type, time]
-#        PTvegzonesTS=np.asarray(PTvegzonesTS)
+#        PTvegzonesSP=np.asarray(PTvegzonesSP)
 
         # READ RFe for each zone and each vegetation
-        RFe_fn = os.path.join(self.MM_ws, inputZON_TS_RFe_fn)
+        RFe_fn = os.path.join(self.MM_ws, inputZON_SP_RFe_fn)
         if os.path.exists(RFe_fn):
             RFetmp = np.loadtxt(RFe_fn)
         else:
             raise ValueError, "\nFATAL ERROR!\nThe file %s doesn't exist!!!" % RFe_fn
-        RFevegzonesTS=np.zeros([NMETEO,NVEG,ntotstp], dtype=float)
+        RFevegzonesSP=np.zeros([NMETEO,NVEG,ntotstp], dtype=float)
         for n in range(NMETEO):
             for v in range(NVEG):
                 for t in range(ntotstp):
-                    RFevegzonesTS[n,v,t]=RFetmp[t+(n*NVEG+v)*ntotstp]
+                    RFevegzonesSP[n,v,t]=RFetmp[t+(n*NVEG+v)*ntotstp]
                     #structure is [number of zones, number of vegetation type, time]
-#        RFevegzonesTS=np.asarray(RFevegzonesTS)
+#        RFevegzonesSP=np.asarray(RFevegzonesSP)
 
 
         # READ PE for each zone and each soil
-        PE_fn = os.path.join(self.MM_ws, inputZON_TS_PE_fn)
+        PE_fn = os.path.join(self.MM_ws, inputZON_SP_PE_fn)
         if os.path.exists(PE_fn):
             PEtmp = np.loadtxt(PE_fn)
         else:
             raise ValueError, "\nFATAL ERROR!\nThe file %s doesn't exist!!!" % PE_fn
-        PEsoilzonesTS=np.zeros([NMETEO,NSOIL,ntotstp], dtype=float)
+        PEsoilzonesSP=np.zeros([NMETEO,NSOIL,ntotstp], dtype=float)
         for n in range(NMETEO):
             for v in range(NSOIL):
                 for t in range(ntotstp):
-                    PEsoilzonesTS[n,v,t]=PEtmp[t+(n*NSOIL+v)*ntotstp]
+                    PEsoilzonesSP[n,v,t]=PEtmp[t+(n*NSOIL+v)*ntotstp]
                     #structure is [number of zones, number of vegetation type, time]
-#        PEsoilzonesTS=np.asarray(PEsoilzonesTS)
+#        PEsoilzonesSP=np.asarray(PEsoilzonesSP)
 
-        return gridVEGarea, RFzonesTS, E0zonesTS, PTvegzonesTS, RFevegzonesTS, PEsoilzonesTS, inputDate, JD
-        del NMETEO, NVEG, NSOIL, inputDate_fn, inputZON_TS_RF_fn, inputZON_TS_PT_fn, inputZON_TS_RFe_fn, inputZON_TS_PE_fn,inputZON_TS_E0_fn
-        del gridVEGarea, RFzonesTS, E0zonesTS, PTvegzonesTS, RFevegzonesTS, PEsoilzonesTS, inputDate
+        return gridVEGarea, RFzonesSP, E0zonesSP, PTvegzonesSP, RFevegzonesSP, PEsoilzonesSP, inputDate, JD
+        del NMETEO, NVEG, NSOIL, inputDate_fn, inputZON_SP_RF_fn, inputZON_SP_PT_fn, inputZON_SP_RFe_fn, inputZON_SP_PE_fn,inputZON_SP_E0_fn
+        del gridVEGarea, RFzonesSP, E0zonesSP, PTvegzonesSP, RFevegzonesSP, PEsoilzonesSP, inputDate
 
     ######################
 

trunk/ppMF_FloPy/ppMODFLOW_flopy_v3.py
@@ -508,19 +508,19 @@ class MF():
 
 ####################################
 
-    def ppMFtime(self,MM_ws, MF_ws, inputDate_fn, inputZON_dTS_RF_fn, inputZON_dTS_PT_fn, inputZON_dTS_RFe_fn, inputZON_dTS_PE_fn, inputZON_dTS_E0_fn, NMETEO, NVEG, NSOIL):
+    def ppMFtime(self,MM_ws, MF_ws, inputDate_fn, inputZON_dSP_RF_fn, inputZON_dSP_PT_fn, inputZON_dSP_RFe_fn, inputZON_dSP_PE_fn, inputZON_dSP_E0_fn, NMETEO, NVEG, NSOIL):
 
         ''' RF analysis to define automatically nper/perlen/nstp
         Daily RF>0 creates a nper
         Succesive days with RF=0 are averaged (PT, PE, E0) to a user-input maximum # of days'''
 
-        def ExportResults1(TS, outFileExport):
+        def ExportResults1(SP, outFileExport):
             """
             Write the processed data in a open txt file readable by  MARMITES
             INPUT:      output flux time series and open file
             """
-            for i in range(len(TS)):
-                out_line =  '%14.9G' %TS[i],'\n'
+            for i in range(len(SP)):
+                out_line =  '%14.9G' %SP[i],'\n'
                 for l in out_line:
                     outFileExport.write(l)
 
@@ -531,15 +531,15 @@ class MF():
         d = []
         for l in inputFile:
             d.append(l)
-        inputFileRF = MMproc.readFile(MM_ws, inputZON_dTS_RF_fn)
+        inputFileRF = MMproc.readFile(MM_ws, inputZON_dSP_RF_fn)
         RF_d = np.zeros([NMETEO, len(d)])
-        inputFilePT = MMproc.readFile(MM_ws, inputZON_dTS_PT_fn)
-        inputFileRFe = MMproc.readFile(MM_ws, inputZON_dTS_RFe_fn)
+        inputFilePT = MMproc.readFile(MM_ws, inputZON_dSP_PT_fn)
+        inputFileRFe = MMproc.readFile(MM_ws, inputZON_dSP_RFe_fn)
         PT_d = np.zeros([NMETEO, NVEG, len(d)])
         RFe_d = np.zeros([NMETEO, NVEG, len(d)])
-        inputFilePE = MMproc.readFile(MM_ws, inputZON_dTS_PE_fn)
+        inputFilePE = MMproc.readFile(MM_ws, inputZON_dSP_PE_fn)
         PE_d = np.zeros([NMETEO, NSOIL, len(d)])
-        inputFileE0 = MMproc.readFile(MM_ws, inputZON_dTS_E0_fn)
+        inputFileE0 = MMproc.readFile(MM_ws, inputZON_dSP_E0_fn)
         E0_d = np.zeros([NMETEO, len(d)])
         for n in range(NMETEO):
             for t in range(len(d)):
@@ -700,28 +700,28 @@ class MF():
                             PE_stp[n,s,sum(nstp[0:per])+stp] += PE_d[n,s,tstart:tend].sum()/(self.perlen[per]/self.nstp[per])
                         E0_stp[n,sum(nstp[0:per])+stp] += E0_d[n,tstart:tend].sum()/(self.perlen[per]/self.nstp[per])
 
-        self.inputZON_TS_RF_fn = "inputZONRF_stp.txt"
-        self.inputZONRF_fn = os.path.join(MM_ws, self.inputZON_TS_RF_fn)
+        self.inputZON_SP_RF_fn = "inputZONRF_stp.txt"
+        self.inputZONRF_fn = os.path.join(MM_ws, self.inputZON_SP_RF_fn)
         inputZONRF = open(self.inputZONRF_fn, 'w')
         inputZONRF.write('#\n')
 
-        self.inputZON_TS_PT_fn = "inputZONPT_stp.txt"
-        self.inputZONPT_fn = os.path.join(MM_ws, self.inputZON_TS_PT_fn)
+        self.inputZON_SP_PT_fn = "inputZONPT_stp.txt"
+        self.inputZONPT_fn = os.path.join(MM_ws, self.inputZON_SP_PT_fn)
         inputZONPT = open(self.inputZONPT_fn, 'w')
         inputZONPT.write('#\n')
 
-        self.inputZON_TS_RFe_fn = "inputZONRFe_stp.txt"
-        self.inputZONRFe_fn = os.path.join(MM_ws, self.inputZON_TS_RFe_fn)
+        self.inputZON_SP_RFe_fn = "inputZONRFe_stp.txt"
+        self.inputZONRFe_fn = os.path.join(MM_ws, self.inputZON_SP_RFe_fn)
         inputZONRFe = open(self.inputZONRFe_fn, 'w')
         inputZONRFe.write('#\n')
 
-        self.inputZON_TS_PE_fn = "inputZONPE_stp.txt"
-        self.inputZONPE_fn = os.path.join(MM_ws, self.inputZON_TS_PE_fn)
+        self.inputZON_SP_PE_fn = "inputZONPE_stp.txt"
+        self.inputZONPE_fn = os.path.join(MM_ws, self.inputZON_SP_PE_fn)
         inputZONPE = open(self.inputZONPE_fn, 'w')
         inputZONPE.write('#\n')
 
-        self.inputZON_TS_E0_fn = "inputZONE0_stp.txt"
-        self.inputZONE0_fn = os.path.join(MM_ws, self.inputZON_TS_E0_fn)
+        self.inputZON_SP_E0_fn = "inputZONE0_stp.txt"
+        self.inputZONE0_fn = os.path.join(MM_ws, self.inputZON_SP_E0_fn)
         inputZONE0 = open(self.inputZONE0_fn, 'w')
         inputZONE0.write('#\n')
 